13e980b9970f9489808a06657e465bb37193ce3f
Aaron Nwabuoku <aerisvulpe@gmail.com>
2021-10-24 19:53:31 -0400

    Include legacy demo

---
+++ src/fragments/Chat.tsx
@@ -0,0 +1,54 @@
+import React, { useContext, useEffect } from 'react';
+import {
+  FlexColumn,
+  Heading,
+  HeadingSizes,
+  StyledBox,
+} from 'react-chat-ui-kit';
+
+import { ChatAppContext } from '../providers/ChatAppProvider';
+
+import ChatHeader from './ChatHeader';
+import ChatMessageInput from './ChatMessageInput';
+import ChatMessages from './ChatMessages';
+
+const Chat: React.FC = () => {
+  const { channel, startChatSession, prependToMessages } =
+    useContext(ChatAppContext);
+
+  useEffect(() => {
+    if (!channel) {
+      return;
+    }
+
+    const session = startChatSession(channel, (message) => {
+      prependToMessages([message]);
+    });
+
+    if (!session) {
+      return;
+    }
+
+    return session.end;
+  }, [channel]);
+
+  return channel ? (
+    <FlexColumn
+      height="100%"
+      width="100%"
+      position={['fixed', 'static']}
+      bg="backgrounds.content"
+      borderRight="light"
+    >
+      <ChatHeader channel={channel} />
+      <ChatMessages channel={channel} />
+      <ChatMessageInput />
+    </FlexColumn>
+  ) : (
+    <StyledBox margin="auto">
+      <Heading size={HeadingSizes.HUGE}>Select channel</Heading>
+    </StyledBox>
+  );
+};
+
+export default Chat;
---
+++ src/fragments/MessageListItem.tsx
@@ -0,0 +1,58 @@
+import { Message as ChatKittyMessage, isUserMessage } from 'chatkitty';
+import moment from 'moment';
+import React, { ReactElement } from 'react';
+import {
+  FlexColumn,
+  FlexRow,
+  Heading,
+  Label,
+  LabelSizes,
+  StyledBox,
+} from 'react-chat-ui-kit';
+import { useHover } from 'react-chat-ui-kit';
+
+import Message from './Message';
+
+interface MessageListItemProps {
+  message: ChatKittyMessage;
+  avatar: ReactElement;
+}
+
+const MessageListItem: React.FC<MessageListItemProps> = ({
+  message,
+  avatar,
+}: MessageListItemProps) => {
+  const sender: { displayName: string } = isUserMessage(message)
+    ? message.user
+    : {
+        displayName: 'ChatKitty',
+      };
+
+  const [isHovering, hoverProps] = useHover({ mouseEnterDelayMS: 0 });
+
+  return (
+    <FlexRow
+      py="1"
+      px={[5, 6]}
+      alignItems="flex-start"
+      bg={isHovering ? 'backgrounds.contentHover' : ''}
+      {...hoverProps}
+    >
+      {avatar}
+      <FlexColumn marginLeft="5" flexGrow={1}>
+        <FlexRow marginBottom="1">
+          <StyledBox marginRight="3">
+            <Heading>{sender.displayName}</Heading>
+          </StyledBox>
+          <Label size={LabelSizes.SMALL}>
+            {moment(message.createdTime).fromNow()}
+          </Label>
+        </FlexRow>
+
+        <Message message={message}></Message>
+      </FlexColumn>
+    </FlexRow>
+  );
+};
+
+export default MessageListItem;
