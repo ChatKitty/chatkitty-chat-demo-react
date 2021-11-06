7a86de1b04d96ef1aefe60ed6f5a12ea1667701f
bowei <bowei.han100@gmail.com>
2021-11-06 14:27:34 -0400

    user presence indicator

--- src/components/CurrentUserDisplay.tsx
+++ src/components/CurrentUserDisplay.tsx
@@ -22,9 +22,16 @@ const CurrentUserDisplay: React.FC<CurrentUserDisplayProps> = ({
           <img src={user.displayPictureUrl} />
         </div>
         <div className="py-2 flex-1 flex flex-col pl-4 border-gray-100 border-b">
-          <h1 className="text-lg font-semibold text-gray-800">
-            {user.displayName}
-          </h1>
+          <div className="flex items-center">
+            <h1 className="text-lg font-semibold text-gray-800">
+              {user.displayName}
+            </h1>
+            <span
+              className={`${
+                user.presence.online ? 'bg-green-500' : 'bg-gray-300'
+              } rounded-full w-2 h-2 ml-2`}
+            ></span>
+          </div>
           <p className="text-sm font-light">Guest</p>
         </div>
         <button className="inline-block sm:hidden p-3" onClick={onClose}>
