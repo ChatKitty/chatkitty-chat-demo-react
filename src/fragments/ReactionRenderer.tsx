32dfa5c4b53aab49966e81a91169b446c3d02739
theonej203 <96204142+theonej203@users.noreply.github.com>
2022-05-11 17:36:52 -0400

    map all the emoji of a message, though need better key value

---
+++ src/fragments/ReactionRenderer.tsx
@@ -0,0 +1,38 @@
+import {  ReactionSummary} from 'chatkitty';
+import { Emoji } from 'emoji-mart';
+import React from 'react';
+import { StyledBox } from 'react-chat-ui-kit';
+
+
+
+interface EmojiProps {
+    reactions: ReactionSummary[] | undefined
+}
+
+const ReactionRenderer: React.FC<EmojiProps> = ({
+    reactions
+}: EmojiProps) => {
+    
+    if (reactions != undefined){
+        reactions[0].emoji
+    }
+
+    return reactions ? (
+        <StyledBox style={{
+            marginTop: '1px',
+            width: '1px',
+            whiteSpace: 'nowrap'
+        }}>
+            {reactions.map((reactionList) =>
+                <div key={reactionList.emoji.character.length}> 
+                    <Emoji size={15} emoji={ reactionList.emoji.character}>t</Emoji>
+                </div>
+            )}
+            
+        </StyledBox>
+        
+    ):(<></>)
+}
+
+
+export default ReactionRenderer;
